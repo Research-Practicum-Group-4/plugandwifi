acaa0973ea97782f1683a0c8dbf3835daa25c605
sunminlee-ucd <sunmin.lee@ucdconnect.ie>
2026-06-08 19:53:03 +0100

    POST /api/auth/login API & JWT Generation

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,25 @@
-from fastapi import FastAPI
-from app.database import engine, Base
+from fastapi import (
+    FastAPI ,
+    Depends,
+    HTTPException
+)
+from .database import (
+    engine, 
+    Base , 
+    get_db
+)
+from .models import User
+from sqlalchemy.orm import Session
+from .schemas import (
+    UserRegister,
+    UserLogin
+)
+from .auth import (
+    hash_password, 
+    verify_password,
+    create_access_token
+)
+>>>>>>> Stashed changes
 
 Base.metadata.create_all(bind=engine)
 
@@ -7,4 +27,93 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Let's get A+!!"}
+
+    return {
+        "message": "Let's get A+!!"
+    }
+
+
+@app.post("/api/auth/register")
+def register_user(
+    payload: UserRegister,
+    db: Session = Depends(get_db)
+):
+    existing_user = db.query(User).filter(
+        User.email == payload.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(
+            status_code = 400,
+            detail = "Email already exists"
+        )
+
+    hashed_pw = hash_password(
+        payload.password
+    )
+
+    new_user = User(
+        full_name = payload.full_name,
+        email = payload.email,
+        password_hash = hashed_pw
+    )
+
+    db.add(new_user)
+
+    db.commit()
+
+    db.refresh(new_user)
+
+    return {
+        "message":
+        "User created successfully"
+    }
+
+@app.post("/api/auth/login")
+def login_user(
+    payload: UserLogin,
+    db:Session = Depends(get_db)
+):
+    user = db.query(User).filter(
+        User.email == payload.email
+    ).first()
+
+    if not user:
+
+        raise HTTPException(
+            status_code=401,
+            detail = "Invalid email or password"
+        )
+    
+    password_correct = verify_password(
+        payload.password,
+        user.password_hash
+    )
+
+    if not password_correct:
+        
+        raise HTTPException(
+            status_code = 401,
+            detail = "Invalid email or password"
+        )
+    
+    access_token = create_access_token(
+
+        {
+            "user_id": user.id,
+            "email": user.email
+        }
+    )
+
+    return {
+
+        "access_token": access_token,
+
+        "token_type": "bearer",
+
+        "user": {
+            "id": user.id,
+            "full_name": user.full_name,
+            "email": user.email
+        }
+    }
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,10 +1,48 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    Column, 
+    Integer, 
+    String,
+    Text,
+    TIMESTAMP,
+    text
+)
 from .database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    full_name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False)
-    password_hash = Column(String, nullable=False)
+    id = Column(
+        Integer,
+        primary_key = True,
+        index = True
+    )
+
+    full_name = Column(
+        Text,
+        nullable = False
+    )
+
+    email = Column(
+        Text,
+        unique = True,
+        nullable = False
+    )
+
+    password_hash = Column(
+        Text,
+        nullable = False
+    )
+
+    role = Column(
+        Text,
+        server_default = "user"
+    )
+
+    created_at = Column(
+        TIMESTAMP,
+        server_default = text(
+            "CURRENT_TIMESTAMP"
+        )
+    )
+
+
