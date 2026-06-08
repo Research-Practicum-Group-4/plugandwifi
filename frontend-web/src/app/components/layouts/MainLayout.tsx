5b3ad09cf39d9c7cb1520812b7498ad88f184de9
peiyouyao <peiyou.yao@ucdconnect.ie>
2026-06-08 14:48:16 +0100

    Done

--- frontend-web/src/app/components/layouts/MainLayout.tsx
+++ frontend-web/src/app/components/layouts/MainLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet, Link, useLocation } from "react-router";
 import { Button } from "../ui/button";
 import { User, Building2 } from "lucide-react";
-import logo from "../../../imports/I_need_a_logo_for_Plug___Wifi__a_mobile_and_web_app_for_users_who_want_to_book_a_table_with_plug___wifi_to_work_during_one_2_or_3_hours_in_cafe___hotel_lobby_or_a_restaurant_for_work_purposes.jpg";
+import logo from "../../../imports/temp_logo.jpg";
 import { ChatBot } from "../ChatBot";
 
 export function MainLayout() {
@@ -13,7 +13,12 @@ export function MainLayout() {
       <header className="border-b border-border bg-card sticky top-0 z-50">
         <div className="container mx-auto px-4 h-16 flex items-center justify-between">
           <Link to="/" className="flex items-center gap-3">
-            <img src={logo} alt="Plug & Wifi" className="h-32" style={{ mixBlendMode: 'multiply' }} />
+            <img
+              src={logo}
+              alt="Plug & Wifi"
+              className="h-32"
+              style={{ mixBlendMode: "multiply" }}
+            />
           </Link>
 
           <nav className="flex items-center gap-4">
